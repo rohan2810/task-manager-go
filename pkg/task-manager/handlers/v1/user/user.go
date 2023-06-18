c8f80002f46bfc0e5cada75a4a246a401b64f1d2
Rohan Surana <rohansurana2810@gmail.com>
2023-06-17 18:02:43 -0700

    update and delete user

--- pkg/task-manager/handlers/v1/user/user.go
+++ pkg/task-manager/handlers/v1/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 	"fmt"
+	"google.golang.org/protobuf/types/known/emptypb"
 	v1 "rohan.com/task-manager/apis/v1/generated"
 	"rohan.com/task-manager/pkg/task-manager/repo/models"
 	"rohan.com/task-manager/pkg/task-manager/repo/postgres"
@@ -81,6 +82,45 @@ func (s *Service) GetUser(ctx context.Context, req *v1.GetUserRequest) (*v1.User
 
 	user, err := s.DB.GetUser(ctx, requestedUsername)
 	if err != nil {
+		return nil, err
+	}
+
+	return &v1.User{
+		Username: user.Username,
+		Fullname: user.Fullname,
+		Email:    user.Email,
+		Role:     user.Role,
+	}, nil
+}
+
+func (s *Service) DeleteUser(ctx context.Context, req *v1.DeleteUserRequest) (*emptypb.Empty, error) {
+	var err error
+	if req == nil {
+		fmt.Println("empty request")
+	}
+	requestedUsername := req.Username
+
+	err = s.DB.DeleteUser(ctx, requestedUsername)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *Service) UpdateUser(ctx context.Context, req *v1.UpdateUserRequest) (*v1.User, error) {
+	var err error
+	if req == nil {
+		fmt.Println("empty request")
+	}
+	userT := &models.User{
+		Username: req.GetUser().Username,
+		Fullname: req.GetUser().Fullname,
+		Email:    req.GetUser().Email,
+		Role:     req.GetUser().Role,
+	}
+	user, err := s.DB.UpdateUser(ctx, userT)
+	if err != nil {
+		fmt.Println(err.Error())
 		return nil, err
 	}
 
